67cae9432e39379bcccfacc1a77a9ee423183439
kaikaibop <grapedinoo@gmail.com>
2022-12-10 16:32:15 -0500

    remove upgradeBoosts

--- Upgrades.java
+++ Upgrades.java
@@ -7,7 +7,6 @@ public class Upgrades {
     // list of upgrades and other upgrade attributes
     private final String[] upgrades = { "Instagram Shoutout", "New Paint", "Sign Flipper", "Youtube Ad",
             "New Furniture", "Better Appliances" };
-    private final int[] upgradeBoost = { 5, 10, 12, 15, 20, 50 };
     private final int[] upgradeCosts = { 10, 20, 30, 50, 100, 250 };
 
     private final int MAX_UPGRADE_LVL = 5;
