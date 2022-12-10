175218a2fee5ef1df7d67cad7decce62aca874bd
kaikaibop <grapedinoo@gmail.com>
2022-12-10 16:00:13 -0500

    yeah

---
+++ Upgrades.java
@@ -0,0 +1,9 @@
+public class Upgrades {
+    // list of upgrades and other upgrade attributes
+    private static final String[] upgrades = { "Instagram Shoutout", "New Paint", "Sign Flipper", "Youtube Ad",
+            "New Furniture", "Better Appliances" };
+    private static final int[] upgradeBoost = { 5, 10, 12, 15, 20, 50 };
+    private static final int[] upgradeCosts = { 10, 20, 30, 50, 100, 250 };
+
+    private static final int MAX_UPGRADE_LVL = 5;
+}
