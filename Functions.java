02e80d59b29a76afe3366ed8da4758445259bb7a
kaikaibop <grapedinoo@gmail.com>
2022-12-10 15:58:35 -0500

    functions

--- Functions.java
+++ Functions.java
@@ -10,6 +10,14 @@ public class Functions {
             "View menu options and user menu", "View all commands for Ramen Bot",
             "Cancel the command" };
 
+    // list of upgrades and other upgrade attributes
+    private static final String[] upgrades = { "Instagram Shoutout", "New Paint", "Sign Flipper", "Youtube Ad",
+            "New Furniture", "Better Appliances" };
+    private static final int[] upgradeBoost = { 5, 10, 12, 15, 20, 50 };
+    private static final int[] upgradeCosts = { 10, 20, 30, 50, 100, 250 };
+
+    private static final int MAX_UPGRADE_LVL = 5;
+
     // displays a help menu
     public static String help() {
         StringBuilder str = new StringBuilder();
