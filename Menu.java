11b03088a63c5bc14bc0f90dda9d248ddd5ca977
kaikaibop <grapedinoo@gmail.com>
2022-12-10 14:05:13 -0500

    array w/ all menu items and return menu item method

--- Menu.java
+++ Menu.java
@@ -1,5 +1,14 @@
 public class Menu {
-  public static void main(String[] args) {
-    
-  }
+
+    // defining all possible menu items
+    private String[] menuItems = {"Beef Ramen", "Seafood Ramen", "Tonkotsu Ramen", "Miso Ramen", "Spicy Miso Ramen", "Soy Sauce (Shoyu) Ramen", "Wasabi Shoyu Ramen", "Pork Rice Bowl", "Japanese Curry Rice Bowl"};
+
+    // returns possible menu item at user-specified index 
+    public String returnMenu(int index) {
+        return menuItems[index];
+    }    
+
+    public static void main(String[] args) {
+
+    }
 }
