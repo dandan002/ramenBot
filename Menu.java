99e27c46f1ce1871d8929e0c147e9ffe30584e15
kaikaibop <grapedinoo@gmail.com>
2022-12-10 14:16:01 -0500

    make returnMenu and returnMenuItem methods

--- Menu.java
+++ Menu.java
@@ -1,12 +1,17 @@
 public class Menu {
 
     // defining all possible menu items
-    private String[] menuItems = {"Beef Ramen", "Seafood Ramen", "Tonkotsu Ramen", "Miso Ramen", "Spicy Miso Ramen", "Soy Sauce (Shoyu) Ramen", "Wasabi Shoyu Ramen", "Pork Rice Bowl", "Japanese Curry Rice Bowl"};
+    private final String[] menuItems = {"Beef Ramen", "Seafood Ramen", "Tonkotsu Ramen", "Miso Ramen", "Spicy Miso Ramen", "Soy Sauce (Shoyu) Ramen", "Wasabi Shoyu Ramen", "Pork Rice Bowl", "Japanese Curry Rice Bowl"};
 
-    // returns possible menu item at user-specified index 
-    public String returnMenu(int index) {
-        return menuItems[index];
+    // returns all possible menu items (as an array) 
+    public String[] returnMenu(int index) {
+        return menuItems;
     }    
+
+    // returns possible menu item at user-specified index
+    public String returnMenuItem(int index) {
+        return menuItems[index];
+    }
 
     public static void main(String[] args) {
 
