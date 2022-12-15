fb9d5a270084dcae29106a585e075184b3b8b256
dandan002 <59246676+dandan002@users.noreply.github.com>
2022-12-15 10:50:11 -0500

    Update DiscordIntegration.java

--- ramenBot/src/main/java/DiscordIntegration.java
+++ ramenBot/src/main/java/DiscordIntegration.java
@@ -11,7 +11,7 @@ public class DiscordIntegration extends ListenerAdapter {
     // Sets up and registers JDA listener
     public static void main(String[] args) throws Exception {
         JDA api = JDABuilder
-                .createDefault("MTA1MDUzNjA0MTQxNDY2MDE1Nw.G49oVh.Gpj4b7S70tQEriVZw_LmHc-moq0fLF-ro5ADkA",
+                .createDefault("MTA1MDUzNjA0MTQxNDY2MDE1Nw.GVZieL.xqBRrVEE24VYBsHquySpHvHPYXy6616-GA2jgI",
                         GatewayIntent.GUILD_MESSAGES, GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MEMBERS)
                 .addEventListeners(new DiscordIntegration()).setActivity(Activity.playing("COS 126")).build();
     }
@@ -19,8 +19,8 @@ public class DiscordIntegration extends ListenerAdapter {
     // Server bot setup
     public void whenJoiningServer(GuildJoinEvent event) {
         TextChannel channel = event.getGuild().getTextChannels().get(0);
-        channel.sendMessage("Thank you for inviting RancidRamen! Please type \"!setPrefix <prefix>\" to set custom prefixes" +
-                "and type \"!help\" for commands and instructions.");
+        channel.sendMessage("Thank you for inviting RancidRamen! Please type \"!setPrefix <prefix>\" to " +
+                "set custom prefixes and type \"!help\" for commands and instructions.");
     }
 
     // check message
@@ -30,25 +30,42 @@ public class DiscordIntegration extends ListenerAdapter {
         if (!event.getAuthor().isBot()) {
             // get ID
             String message = event.getMessage().getContentRaw();
-            System.out.println(message);
             TextChannel currentChannel = event.getChannel().asTextChannel();
             String userId = event.getMember().getId();
             String serverId = event.getGuild().getId();
 
             // check if user exists; if not, add.
             MongoConnection mongoConnection = new MongoConnection(userId, serverId);
-
             // check prefix and workflow
             String prefix = mongoConnection.getPrefix();
-            System.out.println(prefix);
-            Functions workflow = new Functions();
+            Functions workflow = new Functions(mongoConnection);
             if (mongoConnection.getCurrentWorkflow().equals("vc")) {
-                workflow = new FunctionsA();
-            }
-            if (message.equals(prefix + "help")) {
-                currentChannel.sendMessage(workflow.help()).queue();
+                workflow = new FunctionsA(mongoConnection);
             }
 
+            // help
+            if (message.equals(prefix + "help")) {
+                currentChannel.sendMessage(workflow.help(prefix)).queue();
+            }
+
+            // work
+            if (message.equals(prefix + "work")) {
+                currentChannel.sendMessage(workflow.work()).queue();
+                if (mongoConnection.getRamenCooked() > ((int) (100 * mongoConnection.getUserLevel() * 0.75))) {
+                    System.out.println("levelup confirmed");
+                    currentChannel.sendMessage(workflow.levelUp()).queue();
+                }
+            }
+
+            // check balance
+            if (message.equals(prefix + "balance")) {
+                currentChannel.sendMessage(workflow.balance()).queue();
+            }
+
+            // display menu
+            if (message.equals(prefix + "menu")) {
+                currentChannel.sendMessage(workflow.menu()).queue();
+            }    
         }
     }
 }
