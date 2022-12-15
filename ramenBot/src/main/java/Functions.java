05761d7c04206f95e853fb7ce34b0c3e21956f2b
kai <118031472+kaikaibop@users.noreply.github.com>
2022-12-15 16:30:31 -0500

    Update Functions.java

--- ramenBot/src/main/java/Functions.java
+++ ramenBot/src/main/java/Functions.java
@@ -1,4 +1,3 @@
-
 /******************************************************************************
  *
  * This program contains the code behind commands like !balance and !work that
@@ -12,21 +11,35 @@ import static java.lang.System.currentTimeMillis;
 
 public class Functions {
     // list of commands and their descriptions
-    private String[] commands = {"work", "upgrade", "balance", "menu", "help", "rob"};
-    private String[] commandDesc = {"Work for money", "View all purchasable upgrades",
+    private String[] commands = {
+            "work", "upgrade", "balance", "menu",
+            "help", "rob"
+    };
+    private String[] commandDesc = {
+            "Work for money",
+            "View all purchasable upgrades",
             "View user balance",
-            "View menu options and user menu", "View all commands for Ramen Bot",
-            "Attempt to rob someone else"};
+            "View menu options and user menu",
+            "View all commands for Ramen Bot",
+            "Attempt to rob someone else"
+    };
     double oneHr = 3600000;
 
     // list of menu items
-    private final String[] MENU_ITEMS = {"Beef Ramen", "Seafood Ramen", "Tonkotsu Ramen", "Miso Ramen",
-            "Spicy Miso Ramen", "Shoyu Ramen", "Wasabi Shoyu Ramen", "Pork Rice Bowl"};
+    private final String[] MENU_ITEMS = {
+            "Beef Ramen", "Seafood Ramen",
+            "Tonkotsu Ramen", "Miso Ramen",
+            "Spicy Miso Ramen", "Shoyu Ramen",
+            "Wasabi Shoyu Ramen", "Pork Rice Bowl"
+    };
 
     // upgrades
-    private final String[] upgrades = {"Instagram Shoutout", "New Paint", "Sign Flipper", "Youtube Ad",
-            "New Furniture", "Better Appliances"};
-    private final int[] upgradeCosts = {10, 20, 30, 50, 100, 250};
+    private final String[] upgrades = {
+            "Instagram Shoutout", "New Paint",
+            "Sign Flipper", "Youtube Ad",
+            "New Furniture", "Better Appliances"
+    };
+    private final int[] upgradeCosts = { 10, 20, 30, 50, 100, 250 };
     private final int MAX_UPGRADE_LVL = 5;
 
     // passed database connection variable.
@@ -60,7 +73,8 @@ public class Functions {
         user.changeBalance(user.getBalance() + moneyEarned);
         user.changeRamenCooked(user.getRamenCooked() + bowlsSold);
         user.markTime();
-        return "You sold " + bowlsSold + " bowls of ramen and earned $" + moneyEarned;
+        return "You sold " + bowlsSold + " bowls of ramen and earned $"
+                + moneyEarned;
     }
 
     // level up
@@ -75,8 +89,9 @@ public class Functions {
             return "You've reached the max level!";
         }
         StringBuilder str = new StringBuilder();
-        str.append("You leveled up to lvl. " + newLevel + "! You can now cook a max of " +
-                maxCook + " bowls a day!");
+        str.append("You leveled up to lvl. " + newLevel +
+                           "! You can now cook a max of " +
+                           maxCook + " bowls a day!");
         if (newLevel == 20) {
             str.append("\n" + "You're at the max level!");
         }
@@ -119,7 +134,8 @@ public class Functions {
             target.changeBalance(currentTargetBalance - amountStolen);
             user.changeBalance(user.getBalance() + amountStolen);
             return "You stole $" + amountStolen + " from " + name + "!";
-        } else {
+        }
+        else {
             user.changeBalance(user.getBalance() - 300);
             return "You got caught! You were fined $300.";
         }
@@ -133,10 +149,12 @@ public class Functions {
         ArrayList<Integer> currentUpgrades = user.getUpgradeLevels();
         for (int i = 0; i < currentUpgrades.size(); i++) {
             upgrades.append("\n" + (i + 1) + ") " + this.upgrades[i] + " lvl. "
-                    + currentUpgrades.get(i) + " costs " + upgradeCosts[i] + " to upgrade.");
+                                    + currentUpgrades.get(i)
+                                    + " costs " + upgradeCosts[i]
+                                    + " to upgrade.");
         }
         upgrades.append("\n\n" + "To upgrade a function, type " +
-                prefix + "upgrade <UPGRADE NUMBER>");
+                                prefix + "upgrade <UPGRADE NUMBER>");
         return upgrades.toString();
     }
 
@@ -151,8 +169,10 @@ public class Functions {
             user.upgrade(index);
             user.changeBalance(balance - cost);
             user.changeMultiplier(user.getMultiplier() + 0.1);
-            return this.upgrades[index] + " is now level " + user.getUpgradeLevels().get(index);
-        } else {
+            return this.upgrades[index] + " is now level "
+                    + user.getUpgradeLevels().get(index);
+        }
+        else {
             return currentUpgrade + " is already maxed out!";
         }
     }
@@ -160,8 +180,10 @@ public class Functions {
     public static void main(String[] args) {
         MongoConnection kai = new MongoConnection("testUser1", "testServer1");
         Functions kaiw = new Functions(kai);
-        System.out.println(kaiw.help("!")); // prints all callable functions and their descriptions
-        System.out.println(kaiw.work()); // prints "You sold (random number from 1 to 20) bowls
+        System.out.println(kaiw.help("!")); // prints all callable functions
+        // and their descriptions
+        System.out.println(kaiw.work()); // prints "You sold (random number
+        // from 1 to 20) bowls
         // of ramen and earned $(random number * 12)"
 
         kai.changeLevel(19);
@@ -170,14 +192,17 @@ public class Functions {
         // You're at the max level!
         // You unlocked Spicy Miso Ramen!
 
-        System.out.println(kaiw.balance()); // prints You have $(balance) in the bank!
+        System.out.println(kaiw.balance()); // prints You have $(balance)
+        // in the bank!
         System.out.println(kaiw.menu("!")); // prints all current menu items
 
         MongoConnection daniel = new MongoConnection("testUser2", "testServer1");
-        System.out.println(kaiw.rob(daniel, "testUser2")); // prints either a successful
-        // robbery message or "You got caught" message
+        System.out.println(kaiw.rob(daniel, "testUser2")); // prints either a
+        // successful robbery message or "You got caught" message
 
-        System.out.println(kaiw.upgrades("!")); // prints all current upgrades and their levels
-        System.out.println(kaiw.upgrade(0)); // prints "Instagram Shoutout is now level 2"
+        System.out.println(kaiw.upgrades("!")); // prints all current upgrades
+        // and their levels
+        System.out.println(kaiw.upgrade(0)); // prints "Instagram Shoutout
+        // is now level 2"
     }
 }
