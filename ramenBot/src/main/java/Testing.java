bb2638bf3a8e0edf47c8fe169d1a3d6091614b0a
kai <118031472+kaikaibop@users.noreply.github.com>
2022-12-15 13:43:34 -0500

    Create Testing.java

---
+++ ramenBot/src/main/java/Testing.java
@@ -0,0 +1,15 @@
+
+/******************************************************************************
+ *
+ * This program tests methods from the Functions.java file, including things
+ * like printing out the help menu with the specified prefix.
+ *
+ *****************************************************************************/
+
+public class Testing {
+    // calls and tests many of the methods from Functions.java
+    public static void main(String[] args) {
+    
+       // testing help function
+    }
+}
